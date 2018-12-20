766a59ac2e5079e81f5b8a44a2abd67e305a7116
Nikita Zverev <nikita.zverev@konverso.ai>
2018-12-20 17:18:21 +0500

    Added show more\less control to an article

--- src/blog/articles-list/article-list-item.js
+++ src/blog/articles-list/article-list-item.js
@@ -4,9 +4,20 @@ import './article-list-item.scss';
 
 export class ArticleListItem extends Component {
 
+    contentStatuses = ['...more', '...less'];
+    showLessLimit = 50;
+
     constructor(props){
         super(props);
+        this.state = {
+            showMore: false,
+            statusText: this.contentStatuses[0]
+        };
         this.handleButtonClickDelete = this.handleButtonClickDelete.bind(this);
+        this.changeContentStatus = this.changeContentStatus.bind(this);
+        this.getText = this.getText.bind(this);
+        this.getShowMoreControl = this.getShowMoreControl.bind(this);
+        this.isLargeContent = this.isLargeContent.bind(this);
     }
 
     handleButtonClickDelete(event){
@@ -15,11 +26,50 @@ export class ArticleListItem extends Component {
         }
     }
 
+    changeContentStatus(){
+        const newState = {
+            showMore: !this.state.showMore,
+            statusText: this.contentStatuses[+!this.state.showMore]
+        };
+        this.setState(newState);
+    }
+
+    getText(){
+        let result = this.props.text
+            .split(' ')
+            .slice(0, this.showLessLimit)
+            .join(' ');
+
+        if(this.state.showMore){
+            result = this.props.text;
+        }
+        return result;
+    }
+
+    getShowMoreControl(){
+        let control = (
+            <span 
+                className="article-content__show-more"
+                onClick={this.changeContentStatus}>
+                {this.state.statusText}
+            </span>
+        );
+        return this.isLargeContent(this.props.text) ? control : null;
+    }
+
+    isLargeContent(text){
+        const wordsCount = text.split(' ').length;
+        return wordsCount > this.showLessLimit;
+    }
+
     render(){
         return (
             <div className="shadowed article">
                 <div className="article-title">{this.props.title}</div>
-                <div className="article-content">{this.props.text}</div>
+                <div className="article-content">
+                    <span>{this.getText()}</span>
+                    {this.getShowMoreControl()}
+                </div>
                 <div className="article-actions">
                     <Link className="article-actions__item" to={`/articles/${this.props.id}/edit`}>Edit</Link>
                     <button className="article-actions__item" onClick={this.handleButtonClickDelete}>Delete</button>
